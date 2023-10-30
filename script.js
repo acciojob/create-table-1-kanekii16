d1c7341df32c0607870292d540b092ec1e73be6d
kanekii16 <96989943+kanekii16@users.noreply.github.com>
2023-10-30 10:59:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
-  
-  
+
+	const element = document.createElement("tr");
+
+	const td1 = document.createElement("td");
+	td1.innerText = "New Cell 1"
+	const td2 = document.createElement("td");
+	td2.innerText = "New Cell 2";
+
+	element.append(td1,td2);
+
+	const table = document.getElementById("sampleTable");
+
+	table.insertBefore(element,table.children[0]);
+
 }
